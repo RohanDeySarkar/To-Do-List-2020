3bc49d480e1f3f13b5461dd59108ae89f7627720
Aditya-Shrivastava <39914834+Aditya-Shrivastava@users.noreply.github.com>
2020-12-17 20:17:13 +0530

    add encoding

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+
 import './Home.css';
 import HomeSvg from './HomeSvg';
 
 function Home() {
 	const history = useHistory();
 
+	const [{ dispatch, reminders }] = useStateValue();
+
 	useEffect(() => {
 		const token = localStorage.getItem('TOKEN');
 		if (token === null) {
 			history.push('/login');
 		}
+
+		console.log(reminders);
 	}, [history]);
 
 	return (
